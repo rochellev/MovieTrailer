87fc3ec38178cabf035a62e61628cf4ce0fdb447
rochellev <rochellevroberts@utexas.edu>
2018-05-30 16:11:05 -0700

    got toy and disaster images working

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,17 @@
 import fresh_tomatoes
 import media
+""" entertainment_center has all the instances of Movie for the website.  """
 
 #calling the init
-toy_story = media.Movie("Toy Story", "a story boy toy",
+toy_story = media.Movie("Toy Story",
                         "https://vignette.wikia.nocookie.net/disney/images/8/80/Toy_Story_-_Poster.jpg/revision/latest?cb=20150108180742",
                         "https://www.youtube.com/watch?v=4KPTXpQehio") 
 
 
 #print(toy_story.storyline)
 
-avatar = media.Movie("Avatar","alien boys",
-                     "https://en.wikipedia.org/wiki/The_Art_of_Avatar#/media/File:Avatar_picture.jpg",
+avatar = media.Movie("Avatar",
+                     "https://en.wikipedia.org/wiki/The_Disaster_Artist_(film)#/media/File:TheDisastorArtistTeaserPoster.jpg",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 
 #print(avatar.storyline)
@@ -18,10 +19,9 @@ avatar = media.Movie("Avatar","alien boys",
 
 
 disaster_artist = media.Movie("The Disaster Artist",
-                              "misunderstood actor does bad movie",
-                              "https://en.wikipedia.org/wiki/The_Disaster_Artist_(film)#/media/File:TheDisastorArtistTeaserPoster.jpg",
+                              "https://filmwonk.files.wordpress.com/2017/12/the-disaster-artist-poster.jpg?w=807",
                               "https://www.youtube.com/watch?v=4qab3TMg42k")
-#disaster_artist.show_trailer()
+
 
 
 movies = [toy_story, avatar, disaster_artist]
